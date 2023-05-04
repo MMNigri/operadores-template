f0bbb9f6719b9d035c0d9f2e98484caa772704d8
MMN <marcos_marvila@hotmail.com>
2023-05-04 20:59:50 -0300

    Atividade concluida

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,80 @@
 //CODIGO AQUI
+let saldo = 15
+saldo = saldo + 22.40
+//console.log(saldo)
+
+saldo = saldo - 12.34
+//console.log(saldo)
+
+
+saldo = saldo / 2
+
+//console.log(saldo)
+
+saldo = saldo * 3,4
+//console.log(saldo)
+
+saldo = saldo % 3
+console.log(saldo)
+//--------------------------------
+let operacao = 2 * (2 + 2)
+
+console.log(operacao)
+
+//------------------------------------------
+
+const comparacao = 5 == 5
+console.log(comparacao)
+
+//--------------------
+
+const comparacao2 = 5 !== 5
+
+console.log(comparacao2)
+
+//-------------------------------
+
+const comparacao3 = 5 !== "5"
+
+console.log(comparacao3)
+
+//-------------------
+
+const comparacao4 = "5" === "cinco"
+console.log(comparacao4)
+
+//---------------
+
+const comparacao5 = typeof 5 === typeof "20"
+console.log(comparacao5)
+
+//----------------------------
+
+const comparacao6 = typeof "5" === typeof "cinco"
+
+console.log(comparacao6)
+
+//------------------------------
+
+
+const primeiroNumero = 13
+
+const segundoNumero = 13
+
+const verificacao = primeiroNumero === segundoNumero //true
+
+console.log(verificacao)
+
+const verificacao2 = primeiroNumero <= segundoNumero //true
+
+const verificacao3 = primeiroNumero > segundoNumero //false
+
+const verificacao4 = primeiroNumero < segundoNumero //false
+
+
+
+
+
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,7 @@
 // EXERCICIO DE FIXAÇÃO
+let idade0 = Number(prompt("Qual a sua idade"))
+let idade1 = Number(prompt("Qual a idade do seu melhor amigo(a)"))
+
+console.log("Sua idade é maior que a do seu melhor amigo(a)", idade0 > idade1)
+console.log("A sua diferença de idade é:", idade0 - idade1, 'anos')
 
